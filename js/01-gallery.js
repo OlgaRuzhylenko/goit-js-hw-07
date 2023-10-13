ad3fdbe35f1d243789547e21f4bc99c997949f1e
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-10-13 09:35:36 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const container = document.querySelector('.gallery');
+const marcup = galleryItems.map(({ preview, original, description }) =>
+    `<li> 
+    <a href="${original}">
+    <img src="${preview}" alt="${description}" data-source="${original}"/>
+    </a>
+    </li>`).join('')
+container.insertAdjacentHTML('beforeend', marcup);
+
+
+
+
 console.log(galleryItems);
+
