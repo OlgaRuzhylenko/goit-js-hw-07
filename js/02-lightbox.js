5166827d253cba6c6f50ce9c3498f5b4d1452c95
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-10-13 13:14:38 +0300

    2 завдання

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const container = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const marcup = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item"> 
+    <a href="${original}" class="gallery__link">
+    <img src="${preview}" alt="${description}"/>
+    </a>
+    </li>`).join('')
+container.insertAdjacentHTML('beforeend', marcup);
+
+container.addEventListener('click', onLinkClick)
+
+function onLinkClick(evt) {
+    evt.preventDefault();
+    
+    const cardSrc = evt.target.currentSrc;
+   
+    const currentItem = galleryItems.find(({ preview }) => preview === cardSrc)
+    console.log(currentItem);
+    
+    let lightbox = new SimpleLightbox(
+        `<div class="gallery">
+         <a href="${cardSrc}" >
+    <img src="${cardSrc}" alt="${evt.target.getAttribute('alt')}"/>
+    </a>
+       </div>` );
+    lightbox.on('show.simpleLightbox', function(){})
+
+ }
+
+
+
+// console.log(galleryItems);
+
