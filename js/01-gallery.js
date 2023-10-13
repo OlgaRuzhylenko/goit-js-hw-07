a5111913fb62c1529acf5c93e52a6944fed59aff
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-10-13 10:09:35 +0300

    заборона дефолту по лінк

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,13 +3,20 @@ import { galleryItems } from './gallery-items.js';
 
 const container = document.querySelector('.gallery');
 const marcup = galleryItems.map(({ preview, original, description }) =>
-    `<li class="gallery__item"> > 
+    `<li class="gallery__item"> 
     <a href="${original}" class="gallery__link">
     <img src="${preview}" alt="${description}" data-source="${original}"/>
     </a>
     </li>`).join('')
 container.insertAdjacentHTML('beforeend', marcup);
 
+const links = document.querySelectorAll('a')
+
+links.forEach((link) => addEventListener('click', onLinkClick))
+
+function onLinkClick(evt) {
+    evt.preventDefault();
+}
 
 
 // console.log(galleryItems);
