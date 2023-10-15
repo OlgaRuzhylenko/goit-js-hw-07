04a16ec52309d1f76f4d7667d7bdcd9804d260fe
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-10-15 07:18:51 +0300

    2 виправлене

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,31 +2,18 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const container = document.querySelector('.gallery');
 
-const marcup = galleryItems.map(({ preview, original, description }) =>
+const markup = galleryItems.map(({ preview, original, description }) =>
     `<li class="gallery__item"> 
     <a href="${original}" class="gallery__link">
     <img src="${preview}" alt="${description}"/>
     </a>
     </li>`).join('')
-container.insertAdjacentHTML('beforeend', marcup);
-
-container.addEventListener('click', onLinkClick)
-
-function onLinkClick(evt) {
-    evt.preventDefault();
-    
-    const cardSrc = evt.target.currentSrc;
-   
-    const currentItem = galleryItems.find(({ preview }) => preview === cardSrc)
-    console.log(currentItem);
+container.insertAdjacentHTML('beforeend', markup);
 
     const lightbox = new SimpleLightbox('.gallery a', { 
         captionsData: 'alt',
         captionDelay: 250,
     });
-
- }
-
 
 
 // console.log(galleryItems);
